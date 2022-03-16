ee414d2bbfb82ed7a0d88a95550e0d0f26356c4e
Michiboi <emichaud29@gmail.com>
2022-03-16 12:35:15 -0400

    modif config

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # retroLedScreen
 Projet festival
 
+<<<<<<< Updated upstream
 Matrice de LED à utiliser:
 ![image](https://user-images.githubusercontent.com/57765489/158495900-68d39ed6-779c-4c0f-888f-53b29213b41b.png)
 
 Idées de frame:
 ![image](https://user-images.githubusercontent.com/57765489/158496080-ece1ea7f-f6e0-450e-b16a-6dcc32ab3336.png)
+=======
+https://howtomechatronics.com/tutorials/arduino/how-to-control-ws2812b-individually-addressable-leds-using-arduino/
+>>>>>>> Stashed changes
--- retroLedScreen/src/main.cpp
+++ retroLedScreen/src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
+#include <FastLED.h>
+
+#define LED_PIN     7
+#define NUM_LEDS    20
+#define NUM_XLEDS   20
+#define NUM_YLEDS   14
+
+CRGB leds[NUM_LEDS];
+CRGB ledMatrix[NUM_YLEDS][NUM_XLEDS];
 
 void setup() {
-  // put your setup code here, to run once:
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  leds[0] = CRGB(255, 0, 0);
+  FastLED.show();
+  delay(500);  
+  leds[1] = CRGB(0, 255, 0);
+  FastLED.show();
+  delay(500);
+  leds[2] = CRGB(0, 0, 255);
+  FastLED.show();
+  delay(500);
+  leds[5] = CRGB(150, 0, 255);
+  FastLED.show();
+  delay(500);
+  leds[9] = CRGB(255, 200, 20);
+  FastLED.show();
+  delay(500);
+  leds[14] = CRGB(85, 60, 180);
+  FastLED.show();
+  delay(500);
+  leds[19] = CRGB(50, 255, 20);
+  FastLED.show();
+  delay(500);
 }
